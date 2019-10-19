474ad6e26b825437fe56f6945eeb470ab8d8498a
hxh <617662163@qq.com>
2019-10-19 15:11:54 +0800

    开始项目

---
+++ src/main/java/com/hxh/jdsun/JdsunApplication.java
@@ -0,0 +1,13 @@
+package com.hxh.jdsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdsunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdsunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hxh/jdsun/JdsunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxh.jdsun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdsunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
