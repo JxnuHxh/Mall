858b7eaa9a6157fd7618feb436c4251d8b25fd63
hxh <617662163@qq.com>
2019-10-26 11:46:40 +0800

    h

--- src/main/java/com/hxh/jdsun/controller/UserController.java
+++ src/main/java/com/hxh/jdsun/controller/UserController.java
@@ -2,7 +2,13 @@ package com.hxh.jdsun.controller;
 
 import com.hxh.jdsun.util.SendMessageUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -12,10 +18,20 @@ public class UserController {
         return "regTel";
     }
     @RequestMapping("/sendCode")
-    public Object sendCode (){
-        String tel="18379407495";
+    @ResponseBody
+    public Object sendCode (HttpServletRequest request){
+        //服务器端接收客户端请求
+        String tel=request.getParameter("tel");
         String code = SendMessageUtil.getRandomCode(6);
-        int m=SendMessageUtil.send("zgycsmb","d41d8cd98f00b204e980",tel,code);
-        return null;
+        int m=SendMessageUtil.send("zgycsmb","d41d8cd98f00b204e98011",tel,code);
+        String message = SendMessageUtil.getMessage(m);
+        Map<String,Object> map=new HashMap<>();
+        map.put("code",code);
+        map.put("massage","京东"+message);
+        return map;
+    }
+    @RequestMapping("/reg2")
+    public String reg2(){
+        return "reg2";
     }
 }
--- src/main/java/com/hxh/jdsun/exe1026/Exe1.java
+++ src/main/java/com/hxh/jdsun/exe1026/Exe1.java
@@ -1,9 +1,11 @@
 package com.hxh.jdsun.exe1026;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exe1 {
     public static void main(String[] args) {
-        for(int i=0;i<5;i++){
-            System.out.println();
-        }
+        Map<Integer,String> m=new HashMap<>();
+
     }
 }
