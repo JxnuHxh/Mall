213a1eb80f51776587a48c0318d623c279b33c12
hxh <617662163@qq.com>
2019-10-26 09:46:11 +0800

    作业

---
+++ src/main/java/com/hxh/jdsun/exe1026/Exe1.java
@@ -0,0 +1,9 @@
+package com.hxh.jdsun.exe1026;
+
+public class Exe1 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println();
+        }
+    }
+}
